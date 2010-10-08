5cf1974b0b9e0ec4de6bda66027997208693b3ec
Cody Herriges <c.a.herriges@gmail.com>
2010-10-08 14:53:06 -0700

    Non-dynamic resources are in init.pp and syncing script.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # Class: remirror
 #
-# This module manages remirror
+# This module manages creation of a rysyncing service for creating
+# internal mirrors of software.
 #
 # Parameters:
 #
@@ -11,7 +12,38 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class remirror {
+class remirror( $user = "mirror", $group = "nobody", $userdir = "/home/${user}", $base_dest = "/var/remirror" ) {
 
+  user { "${user}":
+    ensure => present,
+    gid => "${group}",
+    homedir => "${userdir}",
+  }
 
+  group { "${group}":
+    ensure => present,
+  }
+
+  file {
+    "${userdir}":
+      ensure => directory,
+      owner => "${user}",
+      group => "$group}",
+      mode  => 700;
+    "${userdir}/local":
+      ensure => directory,
+      owner => "${user}",
+      group => "${group}",
+      mode  => 700;
+    "${userdir}/local/bin":
+      ensure => directory,
+      owner  => "${user}",
+      group  => "${group}",
+      mode   => 700;
+    "${base_dest}":
+      ensure => directory,
+      owner  => "${user}",
+      group  => "${group}",
+      mose   => 755;
+  }
 }
--- manifests/remirror.pp
+++ manifests/remirror.pp
@@ -1,39 +1,19 @@
-define remirror ($syncer = "rsync", $sync_ops = "-rtlvH --progress --stats --delete-after --safe-links", $source_server, $source_path, $sync_minute, $sync_hour, $sync_monthday = "*", $sync_month = "*", $sync_weekday = "*", $destination, $user = "mirror", $group = "nobody", $userdir = "/home/${user}", $ensure = "present") {
-
-  group { "${group}":
-    ensure => present,
-  }
-
-  user { "${user}":
-    gid    => "${group}",
-    ensure => present,
-    home   => "${userdir}",
-  }
+define remirror ($syncer = "rsync", $sync_ops = "-rtlvH --progress --stats --delete-after --safe-links", $source_server, $source_path, $sync_minute, $sync_hour, $sync_monthday = "*", $sync_month = "*", $sync_weekday = "*", $base_dest = "/var/remirror", $user = "mirror", $group = "nobody", $userdir = "/home/${user}", $ensure = "present") {
 
   file {
-    "${userdir}":
-      ensure => directory,
-      owner  => "${user}",
-      group  => "${group}",
-      mode   => 700;
-    "${userdir}/local":
-      ensure => directory,
-      owner  => "${user}",
-      group  => "${group}",
-      mode   => 700;
-    "${userdir}/local/bin":
-      ensure => directory,
-      owner  => "${user}",
-      group  => "${group}",
-      mode   => 700;
-    "${userdir}/local/bin/remirror_${name}.sh"
+    "${userdir}/local/bin/remirror_${name}.sh":
       content => template("${module_name}/remirror_script.erb")
-      mode    => 700,
       owner   => "${user}",
-      group   => "${group}";
+      group   => "${group}",
+      mode    => 700;
+    "${base_dest}/${name}":
+      ensure  => directory,
+      owner   => "${user}",
+      group   => "${group}",
+      mode    => 755;
   }
 
-  cron { "${name}_syncer":
+  cron { "${name}_remirror":
     command  => "${userdir}/local/bin/remirror_${name}.sh",
     ensure   => "${ensure}",
     user     => "${user}",
